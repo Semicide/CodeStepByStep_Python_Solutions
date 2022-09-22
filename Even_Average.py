80cb2b4f80f645094895b92fc66057762cba0275
Semicide <190101033@ogr.atu.edu.tr>
2022-09-22 13:04:43 +0300

    Update Even_Average.py

--- Even_Average.py
+++ Even_Average.py
@@ -1,6 +1,6 @@
 #This code will only count the even numbers and will print their averages
 def Average():
-  y = int(input("Integer? "))
+    y = int(input("Integer? "))
     x = 0
     i = 0
     a = 0
@@ -8,8 +8,8 @@ def Average():
         if y % 2 == 0:
             x += y
             i += 1
-            if (i != 0):
+            if (i!= 0):
                 a = float(x / i)
         y = int(input("Integer? "))
-    return print ("Average: " + str(a))
-    Average()
+    return print("Average: " + str(a))
+Average()

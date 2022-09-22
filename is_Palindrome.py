4abb711ff21417a4c3647dd0f798c7ee2df3e3cc
Semicide <190101033@ogr.atu.edu.tr>
2022-09-22 13:15:48 +0300

    Create is_Palindrome.py

---
+++ is_Palindrome.py
@@ -0,0 +1,7 @@
+#checks if the written string palindrome in the code
+def is_palindrome(s):
+    str = s.lower()
+    for i in range(len(str) // 2):
+        if str[i] != str[- (i + 1)]:
+            return False
+    return True
